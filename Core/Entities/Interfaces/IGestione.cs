c252d24aa02fafb782bfbe79e6eadd5035eaa8ac
Antonio De Luca <antonio.deluca96@gmail.com>
2022-06-21 16:59:52 +0200

    Add project files.

---
+++ Core/Entities/Interfaces/IGestione.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNIBO.Entities.Interfaces
+{
+    public interface IController
+    {
+        public void LogIt(EntryType type, Entry entry);
+
+    }
+
+    public interface IGestioneImpostazioni : IController
+    {
+        public bool CambiaImpostazione(Impostazione impostazione);
+
+
+    }
+}
---
+++ Core/Entities/Model/File.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UNIBO.SET.Model
+{
+    public abstract class File
+    {
+        protected FileStream fs;
+        public abstract FileStream GetFileStream
+        { get; }
+
+
+    }
+
+    public class FileDecifrato : File
+    {
+
+
+        public sealed override FileStream GetFileStream { get { return fs; } }
+        public FileDecifrato(string path)
+        {
+            fs = System.IO.File.Open(path, FileMode.Open, FileAccess.Read); // readOnly
+
+        }
+
+
+    }
+
+    public class FileCifrato : File
+    {
+        public sealed override FileStream GetFileStream { get { return fs; } }
+        public FileCifrato(string path)
+        {
+            fs = System.IO.File.Open(path, FileMode.Open, FileAccess.ReadWrite); // readWrite
+
+        }
+
+
+    }
+}
