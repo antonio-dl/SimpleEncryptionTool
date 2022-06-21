d630d44ceb5e615019364a4c6484a1d09afb7199
jackman91798 <jack.grilli98@gmail.com>
2022-06-21 18:11:24 +0200

    Interfacce

--- Core/Entities/Interfaces/IGestione.cs
+++ Core/Entities/Interfaces/IGestione.cs
@@ -15,7 +15,22 @@ namespace UNIBO.Entities.Interfaces
     public interface IGestioneImpostazioni : IController
     {
         public bool CambiaImpostazione(Impostazione impostazione);
+        public Impostazioni OttieniImpostazioni();
+    }
 
-       
+    public interface IGestioneCredenziali : IController
+    {
+        public void SalvaNuoveCredenziali(Credenziali credenziali);
+    }
+
+    public interface IGestioneLogin : IController
+    {
+        public bool LogIn(Credenziali credenziali);
+    }
+
+    public interface IGestioneVerifica : IController
+    {
+        public void VerificaFile(CodiceVerifica codVerifica, File file);
+        public void GeneraCodice(File file);
     }
 }
