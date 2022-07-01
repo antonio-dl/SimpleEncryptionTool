851638dec0358dd9d96b237debf3e540d1f2d419
jackman91798 <jack.grilli98@gmail.com>
2022-07-01 17:53:12 +0200

    Creazione gestione impostazioni e credenziali presenter

---
+++ Services/Presenters/GestioneImpECredenzialiPresenter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UNIBO.SET.Core.Entities.Interfaces;
+
+namespace UNIBO.SET.Services.Presenters
+{
+    public class GestioneImpECrededenzialiPresenter : IGestioneCredenziali, IGestioneImpostazioni
+    {
+        public void CambiaImpostazione(Impostazione impost)
+        {
+
+        }
+
+        public Impostazioni OttieniImpostazioni()
+        {
+
+        }
+
+        public void SalvaNuoveCredenziali(Credenziali cred)
+        {
+
+        }
+    }
+}
