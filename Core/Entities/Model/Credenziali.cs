25eb0ae4c3e3328f678ce6392845b55fd8a4dedc
Eso98 <marco.esopi@gmail.com>
2022-07-03 16:51:46 +0200

    Implementazione file Credenziali.cs

---
+++ Core/Entities/Model/Credenziali.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNIBO.SET.Model
+{
+    public class CredenzialiPassword : Credenziali
+    {
+        public string password { get; set; }
+
+        public CredenzialiPassword(string psw)
+        {
+            this.password = psw;
+        }
+
+        public override bool Confronta(Credenziali cred)
+        {
+            return this.password.Equals(cred);
+        }
+    }
+}
