fd2ae6121b05fadf0f7c5abe69b705a6c5330d9e
jackman91798 <jack.grilli98@gmail.com>
2022-07-09 16:18:09 +0200

    Creazione test cifratura

---
+++ ServicesTests/Cifratori/CifratoreTestNonCifratoreTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services.Cifratori;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UNIBO.SET.Model;
+namespace Services.Cifratori.Tests
+{
+    [TestClass()]
+    public class CifratoreTestNonCifratoreTests
+    {
+        [TestMethod()]
+        public void CifraFileTest()
+        {
+            File provaFile = new File("C:\\Users\\jackg\\Desktop\\prova.txt");
+            var cifratore = new CifratoreTestNonCifratore();
+            cifratore.CifraFile(provaFile);
+        }
+    }
+}
