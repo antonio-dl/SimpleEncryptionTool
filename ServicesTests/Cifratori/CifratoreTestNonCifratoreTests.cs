1bf7cee1113b613ad62a279edaafcfb98f151133
Antonio De Luca <antonio.deluca96@gmail.com>
2022-07-09 16:43:07 +0200

    modifiche al test

--- ServicesTests/Cifratori/CifratoreTestNonCifratoreTests.cs
+++ ServicesTests/Cifratori/CifratoreTestNonCifratoreTests.cs
@@ -15,9 +15,13 @@ namespace Services.Cifratori.Tests
         [TestMethod()]
         public void CifraFileTest()
         {
-            File provaFile = new File("C:\\Users\\jackg\\Desktop\\prova.txt");
+            string pathfile = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "prova.txt";
+            System.IO.File.Delete(pathfile + ".sef");
+            File provaFile = new File(pathfile);
             var cifratore = new CifratoreTestNonCifratore();
             cifratore.CifraFile(provaFile);
+
+            Assert.IsTrue(System.IO.File.Exists(pathfile + ".sef"));
         }
     }
 }
