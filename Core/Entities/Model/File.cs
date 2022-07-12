56af494a80f97363b9fb8a9dbcaa849b1df744c5
Antonio De Luca <antonio.deluca96@gmail.com>
2022-07-12 12:26:15 +0200

    completato cifratore AEScbc, test passati con successo

--- Core/Entities/Model/File.cs
+++ Core/Entities/Model/File.cs
@@ -1,96 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-
-namespace UNIBO.SET.Model
+﻿namespace UNIBO.SET.Model
 {
-    public class File : FileSystemElement
-    {
-        private readonly System.IO.FileInfo _file;
-
-        public new string Name { get => _file.Name; }
-
-        private CodiceVerifica? _codice = null;
-        public CodiceVerifica CodiceVerifica
-        {
-            get
-            {
-                if (_codice == null)
-                    _codice = new CodiceVerifica(this.CalcolaMD5());
-                return _codice;
-            }
-        }
-
-        private FileStream? _fileStream = null;
-        public FileStream FileStream { get => this._fileStream ??= _file.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite); } // TODO: da eliminare
-
-        public virtual FileStream Open()
-        {
-            return _file.Open(FileMode.Open, FileAccess.Read);
-        }
-
-        public File(string path) : base(path)
-        {
-            _file = new System.IO.FileInfo(path);
-        }
-
-
-        public override IList<string> OttieniPaths()
-        {
-            return new List<string>() { _file.FullName };
-        }
-
-        public string CalcolaMD5()
-        {
-            using (var md5 = System.Security.Cryptography.MD5.Create())
-            {
-                using (var stream = this.FileStream)
-                {
-                    var hash = md5.ComputeHash(stream);
-                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
-                }
-            }
-        }
-    }
-
-    public class FileDecifrato : File
-    {
-
-
-        public FileDecifrato(string path) : base(path)
-        {
-
-
-        }
-
-
-    }
-
-    public class FileCifrato : File
-    {
-        private Key _key;
-
-        public FileCifrato(string path, Key key) : base(path)
-        {
-            _key = key;
-
-        }
-
-
-    }
-
     public class CodiceVerifica
     {
-        public string Codice { get; }
-
         public CodiceVerifica(string codice)
         {
             Codice = codice;
         }
-        
+
+        public string Codice { get; }
         public override bool Equals(object? obj)
         {
             return base.Equals(obj as CodiceVerifica);
@@ -104,6 +21,75 @@ namespace UNIBO.SET.Model
         public override int GetHashCode()
         {
             return HashCode.Combine(Codice);
+        }
+    }
+
+    public class File : FileSystemElement
+    {
+        protected readonly System.IO.FileInfo _file;
+
+        private CodiceVerifica? _codice = null;
+        private FileStream? _fileStream = null;
+        public File(string path) : base(path)
+        {
+            _file = new System.IO.FileInfo(path);
+        }
+
+        public CodiceVerifica CodiceVerifica
+        {
+            get
+            {
+                if (_codice == null)
+                    _codice = new CodiceVerifica(this.CalcolaMD5());
+                return _codice;
+            }
+        }
+
+        public FileStream FileStream { get => this._fileStream ??= _file.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite); }
+        public new string Name { get => _file.Name; }
+        // TODO: da eliminare
+
+        public string CalcolaMD5()
+        {
+            using (var md5 = System.Security.Cryptography.MD5.Create())
+            {
+                using (var stream = this.FileStream)
+                {
+                    var hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
+                }
+            }
+        }
+
+        public virtual FileStream Open()
+        {
+            return _file.Open(FileMode.Open, FileAccess.Read);
+        }
+        public override IList<string> OttieniPaths()
+        {
+            return new List<string>() { _file.FullName };
+        }
+    }
+
+    public class FileCifrato : File
+    {
+        private Key _key;
+
+        public FileCifrato(string path, Key key) : base(path)
+        {
+            _key = key;
+        }
+
+      public FileStream Create()
+        {
+            return this._file.Open(FileMode.Create, FileAccess.Write);
+        }
+    }
+
+    public class FileDecifrato : File
+    {
+        public FileDecifrato(string path) : base(path)
+        {
         }
     }
 }
