0323899e7fea26a753d8d08352703a31908ba7db
jackman91798 <jack.grilli98@gmail.com>
2022-08-26 15:44:22 +0200

    Ho pushatooo

--- SET/Forms/LogView.Designer.cs
+++ SET/Forms/LogView.Designer.cs
@@ -31,7 +31,8 @@
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -60,14 +61,23 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Log";
             // 
-            // label2
+            // comboBox2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(49, 107);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(110, 20);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Log visualizzati";
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(358, 57);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(206, 28);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(358, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tipo di operazione";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // LogView
             // 
@@ -75,7 +85,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PowderBlue;
             this.ClientSize = new System.Drawing.Size(802, 467);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.richTextBox1);
@@ -91,6 +102,7 @@
         private RichTextBox richTextBox1;
         private ComboBox comboBox1;
         private Label label1;
-        private Label label2;
+        private ComboBox comboBox2;
+        private Label label3;
     }
 }
