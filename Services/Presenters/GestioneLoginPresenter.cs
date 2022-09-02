781242f578f7bd1f5df68d0145e5c1c0f8feda9a
Eso98 <marco.esopi@gmail.com>
2022-09-02 18:35:39 +0200

    Impostati 3 secondi per fare il login invece di 1 che era prima

--- Services/Presenters/GestioneLoginPresenter.cs
+++ Services/Presenters/GestioneLoginPresenter.cs
@@ -18,7 +18,7 @@ namespace UNIBO.SET.Services.Presenters
 
         public bool LogIn(Credenziali credenziali)
         {
-            Thread.Sleep(1000); // Per evitare brute force
+            Thread.Sleep(3000); // Per evitare brute force
             if(!_utente.Credenziali.Confronta(credenziali))
             {
                 LogIt(EntryType.Avvertimento, "Login fallito, credenziali errate");
