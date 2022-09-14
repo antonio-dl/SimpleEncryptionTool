b3faa01a92f69c4de725321cbff19b595a12e70e
jackman91798 <jack.grilli98@gmail.com>
2022-09-14 12:46:34 +0200

    Get dei presenters in inizializzatore

--- Services/Presenters/Inizializzatore.cs
+++ Services/Presenters/Inizializzatore.cs
@@ -9,12 +9,18 @@ namespace Services.Presenters
 {
     public class Inizializzatore // Classe che si occupa di effettuare tutte le iniezioni di dipendenza
     {
-        private GestioneCifraturaPresenter _cifratura;
-        private GestioneDecifraturaPresenter _decifratura;
-        private GestioneImpECrededenzialiPresenter _impECrededenziali;
-        private GestioneLoginPresenter _login;
-        private GestioneLogPresenter _log;
-        private GestioneVerificaPresenter _verifica;
+        private GestioneCifraturaPresenter _gestioneCifraturaPresenter;
+        private GestioneDecifraturaPresenter _gestioneDecifraturaPresenter;
+        private GestioneImpECrededenzialiPresenter _gestioneImpECrededenzialiPresenter;
+        private GestioneLoginPresenter _gestioneLoginPresenter;
+        private GestioneLogPresenter _gestioneLogPresenter;
+        private GestioneVerificaPresenter _gestioneVerificaPresenter;
 
+        public GestioneCifraturaPresenter GestioneCifraturaPresenter { get => _gestioneCifraturaPresenter; }
+        public GestioneDecifraturaPresenter GestioneDecifraturaPresenter { get => _gestioneDecifraturaPresenter; }
+        public GestioneImpECrededenzialiPresenter GestioneImpECrededenzialiPresenter { get => _gestioneImpECrededenzialiPresenter; }
+        public GestioneLoginPresenter GestioneLoginPresenter { get => _gestioneLoginPresenter; }
+        public GestioneLogPresenter GestioneLogPresenter { get => _gestioneLogPresenter; }
+        public GestioneVerificaPresenter GestioneVerificaPresenter { get => _gestioneVerificaPresenter; }
     }
 }
