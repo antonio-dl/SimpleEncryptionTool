6edb93eec29a953879d78d4ccd7811eb84825e25
Antonio De Luca <antonio.deluca96@gmail.com>
2022-09-14 12:43:07 +0200

    cambiato posizione classe credenziali nel file appropriato

--- Services/Presenters/Inizializzatore.cs
+++ Services/Presenters/Inizializzatore.cs
@@ -8,7 +8,22 @@ namespace Services.Presenters
 {
     public class Inizializzatore // Classe che si occupa di effettuare tutte le iniezioni di dipendenza
     {
-        
+        public Inizializzatore()
+        {
+            CreaUtenteSingleton();
+            CaricaComponenti();
 
+
+        }
+
+        private void CaricaComponenti()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreaUtenteSingleton()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
