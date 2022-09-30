431eed067be8688dd61621d2121449afde73126c
Antonio De Luca <antonio.deluca96@gmail.com>
2022-09-30 12:34:53 +0200

    sopostate variabili di ambiente

--- Services/Environment/Environment.cs
+++ Services/Environment/Environment.cs
@@ -1,6 +1,6 @@
 ﻿namespace UNIBO.SET
 {
-    internal class SETEnvironment
+    public class SETEnvironment
     {
         public static readonly string Configuration_Path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SET", "settings.json");
         public static readonly string Credential_Path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SET", "cred.bin");
