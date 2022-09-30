012d1d78ded2ce049dd984518fc4a5f74546fcc4
Eso98 <marco.esopi@gmail.com>
2022-09-30 16:50:46 +0200

    LogView completa, in teoria funzionante

--- SET/Forms/LogView.cs
+++ SET/Forms/LogView.cs
@@ -24,22 +24,58 @@ namespace UNIBO.SET.GUI.Forms
         private void LogView_Load(object sender, EventArgs e)
         {
             var logFiles = _presenter.GetAllLogs();
-            LogBox.DataSource = logFiles;
+            this.LogBox.DataSource = logFiles;
 
             List<object> list = new List<object>();
             list.Add("Tutto");
             list.AddRange(Enum.GetNames<EntryType>());
-            TipoBox.DataSource = list;
+            this.TipoBox.DataSource = list;
+
+            this.LogBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            this.TipoBox.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            this.richTextBox1.Clear();
+            this.richTextBox1.ReadOnly = true;
         }
 
         private void LogBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            Log logSelezionato;
+            EntryType tipo;
+            string result = "";
+            if(LogBox.SelectedItem is not null && TipoBox.SelectedItem is null)
+            {
+                logSelezionato = (Log) LogBox.SelectedItem;
+                result = _presenter.ReadLog(logSelezionato);
+            }
+            else if (LogBox.SelectedItem is not null && TipoBox.SelectedItem is not null)
+            {
+                logSelezionato = (Log)LogBox.SelectedItem;
+                tipo = (EntryType)TipoBox.SelectedItem;
+                result = _presenter.ReadLog(logSelezionato, tipo);
+            }
+            richTextBox1.Clear();
+            richTextBox1.Text = result;
         }
 
         private void TipoBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            Log logSelezionato;
+            EntryType tipo;
+            string result = "";
+            if (LogBox.SelectedItem is not null && TipoBox.SelectedItem is not null)
+            {
+                logSelezionato = (Log) LogBox.SelectedItem;
+                tipo = (EntryType) TipoBox.SelectedItem;
+                result = _presenter.ReadLog(logSelezionato, tipo);
+            }
+            else if (LogBox.SelectedItem is not null && TipoBox.SelectedItem is null)
+            {
+                logSelezionato = (Log)LogBox.SelectedItem;
+                result = _presenter.ReadLog(logSelezionato);
+            }
+            richTextBox1.Clear();
+            richTextBox1.Text = result;
         }
     }
 }
